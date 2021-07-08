f01b2804de3a02d12c8e96f80cda1ee3ba6c79b9
imad <imadatyatalah@gmail.com>
2021-07-08 21:12:08 +0100

    feat: Wrap BoxComponent with OctocakeProvider

--- src/components/box/Box.stories.js
+++ src/components/box/Box.stories.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import OctocakeProvider from "../../theme/OctocakeProvider";
+
 import BoxComponent from "./Box";
 
 export default {
@@ -8,7 +10,11 @@ export default {
   argTypes: {},
 };
 
-const Template = (args) => <BoxComponent {...args} />;
+const Template = (args) => (
+  <OctocakeProvider>
+    <BoxComponent {...args} />
+  </OctocakeProvider>
+);
 
 export const Box = Template.bind({});
 Box.args = {
