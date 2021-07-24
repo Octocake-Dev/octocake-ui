c408575827c5a306a4d448611a12c4c2f1ea6083
imad <imadatyatalah@gmail.com>
2021-07-24 12:04:54 +0100

    feat: Setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import { terser } from "rollup-plugin-terser";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { babel } from "@rollup/plugin-babel";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/index.es.js",
+      format: "es",
+      exports: "named",
+    },
+  ],
+  plugins: [
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-react"],
+    }),
+    peerDepsExternal(),
+    nodeResolve(),
+    terser(),
+  ],
+};
