92afc16aefefa2592d60ddb73dd4934bd394edd1
imad <imadatyatalah@gmail.com>
2021-07-25 12:19:42 +0100

    chore: Added & Configue rollup-plugin-dts

--- rollup.config.js
+++ rollup.config.js
@@ -4,35 +4,44 @@ import { babel } from "@rollup/plugin-babel";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.js",
-      format: "cjs",
-    },
-    {
-      file: "dist/index.es.js",
-      format: "es",
-      exports: "named",
-    },
-  ],
-  plugins: [
-    commonjs(),
-    babel({
-      babelHelpers: "bundled",
-      exclude: "node_modules/**",
-      presets: ["@babel/preset-react"],
-    }),
-    peerDepsExternal(),
-    nodeResolve(),
-    terser(),
-    typescript({
-      useTsconfigDeclarationDir: true,
-      tsconfigOverride: {
-        exclude: ["**/*.stories.*"],
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
       },
-    }),
-  ],
-};
+      {
+        file: "dist/index.es.js",
+        format: "es",
+        exports: "named",
+      },
+    ],
+    plugins: [
+      commonjs(),
+      babel({
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
+      }),
+      peerDepsExternal(),
+      nodeResolve(),
+      terser(),
+      typescript({
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: {
+          exclude: ["**/*.stories.*"],
+        },
+      }),
+    ],
+  },
+
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
