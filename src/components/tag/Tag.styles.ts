107d8e493aaa6920dd4cbddcb8384ef266c70be4
imad <imadatyatalah@gmail.com>
2021-07-25 17:57:52 +0100

    feat: Added styles for tag component #16

--- src/components/tag/Tag.styles.ts
+++ src/components/tag/Tag.styles.ts
@@ -3,4 +3,41 @@ import styled from "styled-components";
 
 import { TagProps } from "../../types/tag";
 
-export const StyledTag = styled("span")<TagProps>({}, variant({}));
+export const StyledTag = styled("span")<TagProps>(
+  {
+    borderRadius: "5px",
+    display: "inline-flex",
+    verticalAlign: "top",
+    alignItems: "center",
+    maxWidth: "100%",
+    fontSize: "15px",
+    fontWeight: 500,
+  },
+
+  (props) =>
+    variant({
+      prop: "colorScheme",
+      variants: {
+        primary: {
+          backgroundColor: props.theme.colors.primary[100],
+          color: props.theme.colors.primary[900],
+          border: `1px solid ${props.theme.colors.primary[500]}`,
+        },
+      },
+    }),
+
+  variant({
+    prop: "size",
+    variants: {
+      sm: {
+        padding: "0.3rem 0.8rem",
+      },
+      md: {
+        padding: "0.4rem 1rem",
+      },
+      lg: {
+        padding: "0.5rem 1.2rem",
+      },
+    },
+  })
+);
--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -3,8 +3,14 @@ import React from "react";
 import { StyledTag } from "./Tag.styles";
 import { TagProps } from "../../types/tag";
 
-const Tag = ({ children, ...props }: TagProps) => (
-  <StyledTag {...props}>{children}</StyledTag>
-);
+const Tag = (props: TagProps) => {
+  const { children, size = "md", colorScheme = "primary" } = props;
+
+  return (
+    <StyledTag size={size} colorScheme={colorScheme} {...props}>
+      {children}
+    </StyledTag>
+  );
+};
 
 export default Tag;
