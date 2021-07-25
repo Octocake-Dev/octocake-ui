b376c89ac457fc1862cd4c103ab634a8fc8cf821
imad <imadatyatalah@gmail.com>
2021-07-25 18:08:44 +0100

    Import Tag component #16

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export { default as Button } from "./components/button/Button";
 export { default as OctocakeProvider } from "./theme/OctocakeProvider";
+export { default as Button } from "./components/button/Button";
+export { default as Tag } from "./components/tag/Tag";
