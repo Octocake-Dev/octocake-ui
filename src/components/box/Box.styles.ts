6c83d98ca3c82e8c0247f0068adf7d2ff42e7950
imad <imadatyatalah@gmail.com>
2021-07-26 20:56:53 +0100

    Remove Box component

--- src/components/box/Box.styles.ts
+++
@@ -1,6 +0,0 @@
-import { variant } from "styled-system";
-import styled from "styled-components";
-
-import { BoxProps } from "../../types/box";
-
-export const StyledBox = styled("div")<BoxProps>({}, variant({}));
