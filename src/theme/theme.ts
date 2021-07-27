da68f5226f9ac4642e061498f9f31d57d9cd46d4
imad <imadatyatalah@gmail.com>
2021-07-27 22:22:23 +0100

    fix: Replace colors object

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,5 +1,18 @@
-import colors from "./colors";
-
-const theme = { colors };
+const theme = {
+  colors: {
+    primary: {
+      50: "#99e2b4",
+      100: "#88d4ab",
+      200: "#78c6a3",
+      300: "#67b99a",
+      400: "#56ab91",
+      500: "#469d89",
+      600: "#358f80",
+      700: "#248277",
+      800: "#14746f",
+      900: "#036666",
+    },
+  },
+};
 
 export default theme;
